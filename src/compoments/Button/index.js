309ac7849e8e5a8772c3c42309323d0b7eaa8bfd
tranquangthagn <83894128+tranquangthagn@users.noreply.github.com>
2022-07-09 08:44:37 +0700

    Cau hinh #2

---
+++ src/compoments/Button/index.js
@@ -0,0 +1,9 @@
+import { MyAlert } from "~/myAlert";
+
+function Button() {
+    return ( 
+        <button onClick={MyAlert}>Click me</button>
+     );
+}
+
+export default Button;
---
+++ src/myAlert.js
@@ -0,0 +1,3 @@
+export const MyAlert = () => {
+    console.log('tran thang');
+};
